b8029ac84bacdd80e7a20097410079ba03a7e525
Jagruti Akkisetty <110735182+7j7a7@users.noreply.github.com>
2023-11-02 20:22:02 +0000

    Update Script.js

--- Script.js
+++ Script.js
@@ -2,6 +2,7 @@ function bOne() {
     alert("1");
 }
 
+
 function bTwo() { 
     document.getElementById("body").style.backgroundColor = "#4D3A4D";
     document.getElementById("nav").style.backgroundColor = "#3c303c";
@@ -20,10 +21,37 @@ function bTwo() {
 function bThree() { 
     document.getElementById("body").style.backgroundColor = "#FBF4D7";
     document.getElementById("nav").style.backgroundColor = "#4D3A4D";
+
+    const textCol = document.querySelectorAll("h1, p");
+    textCol.forEach((element) => {
+        element.style.color = "black";
+    });
+
+    const txtCol = document.querySelectorAll("li, button");
+    txtCol.forEach((element) => {
+        element.style.color = "#FBF4D7";
+    });
    
     const bCol = document.querySelectorAll("button");
     bCol.forEach((element) => {
         element.style.backgroundColor = "#86007C";
     });
 
+}
+//single line
+/*multi line*/
+
+function bFour() {
+    var textBox = document.getElementById("textbox");
+    textBox.style.fontSize="30pt";
+    textBox.style.fontFamily="Comic Sand MS";
+    textBox.style.color="red";
+}
+
+function bFive() {
+    confirm("messages");
+}
+
+function bSix() {
+    prompt("message");
 }
